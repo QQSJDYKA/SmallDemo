7fd11def23e45e2242c2322a7c56ea4e9533fb69
halo <1434508018@qq.com>
2019-02-20 14:30:29 +0800

    222

--- app.word/src/main/java/com/eebbk/smalldemo/app/word/MainActivity.java
+++ app.word/src/main/java/com/eebbk/smalldemo/app/word/MainActivity.java
@@ -6,10 +6,13 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 
-import com.eebbk.smalldemo.app.word.bean.BundleJsonBean;
 import com.google.gson.Gson;
 
 import net.wequick.small.Small;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -32,14 +35,14 @@ public class MainActivity extends Activity {
         findViewById(R.id.button_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Small.openUri("main", MainActivity.this);
+                Small.openUri("main/main2", MainActivity.this);
             }
         });
         findViewById(R.id.button_2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final OkHttpClient httpClient = new OkHttpClient();
-                Request request = new Request.Builder()
+                final Request request = new Request.Builder()
                         .url("https://github.com/QQSJDYKA/SmallDemo/raw/master/update/bundle.json")
                         .method("GET", null)
                         .build();
@@ -53,42 +56,52 @@ public class MainActivity extends Activity {
                     @Override
                     public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                         Gson gson = new Gson();
-                        BundleJsonBean bundleJsonBean = gson.fromJson(response.body().string(), BundleJsonBean.class);
-                        if (bundleJsonBean.getManifest() != null) {
-                            if (Small.updateManifest(bundleJsonBean.getManifest(), false)) {
-                                Log.i("haloq", "更新失败");
-                                return;
-                            }
-                        }
-                        for (final BundleJsonBean.UpdatesBean bean : bundleJsonBean.getUpdates()) {
-                            Request request1 = new Request.Builder()
-                                    .url(bean.getUrl())
-                                    .get().build();
-                            httpClient.newCall(request1)
-                                    .enqueue(new Callback() {
-                                        @Override
-                                        public void onFailure(Call call, IOException e) {
-                                            Log.i("haloq", bean.getPkg() + "更新失败");
-                                        }
 
-                                        @Override
-                                        public void onResponse(Call call, Response response) throws IOException {
-                                            net.wequick.small.Bundle bundle = Small.getBundle(bean.getPkg());
-                                            File file = bundle.getPatchFile();
-                                            InputStream inputStream = response.body().byteStream();
-                                            OutputStream os = new FileOutputStream(file);
-                                            byte[] buffer = new byte[1024];
-                                            int length;
-                                            while ((length = inputStream.read(buffer)) != -1) {
-                                                os.write(buffer, 0, length);
+                        try {
+                            JSONObject bundleJson = new JSONObject(response.body().string());
+                            JSONObject manifest = bundleJson.optJSONObject("manifest");
+                            if (manifest != null) {
+                                if (!Small.updateManifest(manifest, false)) {
+                                    Log.i("haloq", "更新失败");
+                                    return;
+                                }
+                            }
+                            JSONArray updates = bundleJson.getJSONArray("updates");
+
+                            for (int i = 0; i < updates.length(); i++) {
+                                JSONObject jsonObject = updates.getJSONObject(i);
+                                final String packageName = jsonObject.optString("pkg", "");
+                                Request request1 = new Request.Builder()
+                                        .url(jsonObject.optString("url"))
+                                        .get().build();
+                                httpClient.newCall(request1)
+                                        .enqueue(new Callback() {
+                                            @Override
+                                            public void onFailure(Call call, IOException e) {
+                                                Log.i("haloq", packageName + "更新失败");
                                             }
-                                            os.flush();
-                                            os.close();
-                                            inputStream.close();
-                                            bundle.upgrade();
-                                            Log.i("haloq", bean.getPkg() + "更新完成");
-                                        }
-                                    });
+
+                                            @Override
+                                            public void onResponse(Call call, Response response) throws IOException {
+                                                net.wequick.small.Bundle bundle = Small.getBundle(packageName);
+                                                File file = bundle.getPatchFile();
+                                                InputStream inputStream = response.body().byteStream();
+                                                OutputStream os = new FileOutputStream(file);
+                                                byte[] buffer = new byte[1024];
+                                                int length;
+                                                while ((length = inputStream.read(buffer)) != -1) {
+                                                    os.write(buffer, 0, length);
+                                                }
+                                                os.flush();
+                                                os.close();
+                                                inputStream.close();
+                                                bundle.upgrade();
+                                                Log.i("haloq", packageName + "更新完成");
+                                            }
+                                        });
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
                     }
                 });
---
+++ app.word/src/main/java/com/eebbk/smalldemo/app/word/bean/UpdatesBean.java
@@ -0,0 +1,33 @@
+package com.eebbk.smalldemo.app.word.bean;
+
+/**
+ * Author: halo_huang
+ * Create: 2019/2/20
+ * Describe:
+ */
+public class UpdatesBean {
+    /**
+     * pkg : com.eebbk.smalldemo.app.word
+     * url : https://github.com/QQSJDYKA/SmallDemo/update/libcom_eebbk_smalldemo_app_word.so
+     */
+
+    private String pkg;
+    private String url;
+
+    public String getPkg() {
+        return pkg;
+    }
+
+    public void setPkg(String pkg) {
+        this.pkg = pkg;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
